6f3dff4fdb36810c52aa0818e94612d776b96638
melius19 <melius19@protonmail.com>
2018-01-25 11:45:43 +0900

    ok

--- src/test/java/org/zerock/test/BoardDAOTest.java
+++ src/test/java/org/zerock/test/BoardDAOTest.java
@@ -1,6 +1,6 @@
 package org.zerock.test;
 
-import static org.junit.Assert.fail;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -10,12 +10,17 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 import org.zerock.domain.BoardVO;
+import org.zerock.domain.Criteria;
 import org.zerock.persistence.BoardDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/**/root-context.xml" })
 public class BoardDAOTest {
+
+	int num = 4;
 
 	@Inject
 	private BoardDAO dao;
@@ -33,13 +38,13 @@ public class BoardDAOTest {
 
 	@Test
 	public void testRead() throws Exception {
-		logger.info(dao.read(1).toString());
+		logger.info(dao.read(num).toString());
 	}
 
 	@Test
 	public void testUpdate() throws Exception {
 		BoardVO board = new BoardVO();
-		board.setBno(1);
+		board.setBno(num);
 		board.setTitle("수정된 글입니다.");
 		board.setContent("수정 테스트");
 		dao.update(board);
@@ -47,6 +52,47 @@ public class BoardDAOTest {
 
 	@Test
 	public void testDelete() throws Exception {
-		dao.delete(1);
+		dao.delete(num);
+	}
+
+	@Test
+	public void testListPage() throws Exception {
+		int page = 3;
+		List<BoardVO> list = dao.listPage(page);
+
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
+
+	@Test
+	public void testListCriteria() throws Exception {
+		Criteria cri = new Criteria();
+		cri.setPage(2);
+		cri.setPerPageNum(20);
+
+		List<BoardVO> list = dao.listCriteria(cri);
+
+		for (BoardVO boardVO : list) {
+			logger.info(boardVO.getBno() + ":" + boardVO.getTitle());
+		}
+	}
+
+	@Test
+	public void testURI() throws Exception {
+		UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/board/read").queryParam("bno", 12)
+				.queryParam("perPageNum", 20).build();
+
+		logger.info("/board/read?bno=12&perPageNum=20");
+		logger.info(uriComponents.toString());
+	}
+
+	@Test
+	public void testURI2() throws Exception {
+		UriComponents uriComponents = UriComponentsBuilder.newInstance().path("/{module}/{page}").queryParam("bno", 12)
+				.queryParam("perPageNum", 20).build().expand("board", "read").encode();
+
+		logger.info("/board/read?bno=12&perPageNum=20");
+		logger.info(uriComponents.toString());
 	}
 }
